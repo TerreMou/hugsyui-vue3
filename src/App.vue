723cf479e29c7aa0e6322103cd8be0bfe2ea11b7
terremou <terrence_mou@foxmail.com>
2022-01-18 15:16:30 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Terre/>
+</template>
+
+<script>
+import Terre from './components/Terre.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Terre
+  }
+}
+</script>
