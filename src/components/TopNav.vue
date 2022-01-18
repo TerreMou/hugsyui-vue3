e291ce31906cf43c4ed2bc7cea951c112b0821c7
terremou <terrence_mou@foxmail.com>
2022-01-18 22:12:44 +0800

    完成 TopNav 响应式

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleAside">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside" @click="toggleAside"></span>
   </div>
 </template>
 
@@ -13,13 +14,13 @@ import {inject, Ref} from "vue";
 
 export default {
   setup() {
-    let asideVisible = inject<Ref<boolean>>('asideVisible') //get
+    let asideVisible = inject<Ref<boolean>>("asideVisible"); //get
     const toggleAside = () => {
-      asideVisible.value = !asideVisible.value
-    }
-    return {toggleAside}
+      asideVisible.value = !asideVisible.value;
+    };
+    return {toggleAside};
   }
-}
+};
 
 </script>
 
@@ -30,20 +31,39 @@ export default {
   background: pink;
   display: flex;
   padding: 16px;
+  justify-content: center;
+  align-items: center;
 
-> .logo {
+  > .logo {
     max-width: 6em;
     margin-right: auto;
   }
 
-> .menu {
+  > .menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
 
-  > li {
+    > li {
       margin: 0 1em;
     }
+  }
+
+  > .toggleAside {
+    display: none;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  @media (max-width: 500px) {
+    > .menu {display: none}
+    > .logo {margin: 0 auto;}
+    > .toggleAside {display: inline-block;}
   }
 }
 </style>
