1cd8727caea108a143d83fb05100a544c11b9d85
terremou <terrence_mou@foxmail.com>
2022-01-19 23:57:00 +0800

    创建 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    Dialog 示例
-  </div>
+  <h2>Dialog 对话框</h2>
+  <h3>示例1</h3>
+  <Dialog :visible="true" title="标题"
+          @yes="fn1" @no="fn2">
+
+  </Dialog>
 </template>
+<script>
+import Dialog from '../lib/Dialog.vue';
+
+export default {
+  components: {Dialog}
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hugsy-dialog-overlay"/>
+  <div class="hugsy-dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行字</p>
+      <p>第二行字</p>
+    </main>
+    <footer>
+      <Button>OK</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+  components: {Button},
+  props: {
+    visible: Boolean,
+    title: String,
+  },
+}
+</script>
