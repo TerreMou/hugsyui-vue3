97bd52b19374d419f6005877145408e8d400b041
terremou <terrence_mou@foxmail.com>
2022-01-22 02:09:52 +0800

    提取 Layout 组件

---
+++ src/components/Layout.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1>{{ mainHeading }}</h1>
+    <p>{{ mainDescription }}</p>
+  </header>
+  <section>
+    <h2>{{ subHeading }}</h2>
+    <slot/>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    mainHeading: String,
+    mainDescription: String,
+    subHeading: String
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  padding: 16px 16px;
+
+  h1 {
+    font-size: 36px;
+    font-weight: 600;
+  }
+}
+section {
+  h2 {
+    font-size: 28px;
+    font-weight: 600;
+    padding: 16px 16px 0 16px;
+  }
+}
+</style>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
-    <div class="title">
-      <h1>Switch 开关</h1>
-      <p>Switch component turns on or off the state of your custom option</p>
-    </div>
-    <section>
-      <h2>示例</h2>
+    <Layout main-heading="Switch 开关"
+            main-description="Switch component turns on or off the state of your custom option"
+            sub-heading="示例">
       <Demo :component="Switch1Demo"
             description="This is basic usage of switch component."/>
       <Demo :component="Switch2Demo"
             description="Switch can be in disabled state."/>
-    </section>
+    </Layout>
   </div>
 </template>
 
@@ -18,31 +15,12 @@
 import Switch1Demo from './code/Switch1.demo.vue';
 import Switch2Demo from './code/Switch2.demo.vue';
 import Demo from './Demo.vue';
+import Layout from './Layout.vue';
 
 export default {
-  components: {Demo},
+  components: {Layout, Demo},
   setup() {
     return {Switch1Demo, Switch2Demo};
   }
 };
 </script>
-
-<style lang="scss" scoped>
-
-.title {
-  padding: 16px 16px;
-  h1 {
-    font-size: 36px;
-    font-weight: 600;
-  }
-}
-
-section {
-  h2 {
-    font-size: 28px;
-    font-weight: 600;
-    padding: 16px 16px 0 16px;
-  }
-}
-
-</style>
